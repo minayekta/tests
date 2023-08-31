70672899a6c4343cc01cb686667deb1c62206e33
Zokaee <Zokaee.A@ronixtools.com>
2023-08-31 12:02:54 +0330

    save me

--- js/script.js
+++ js/script.js
@@ -1,100 +1,216 @@
-$ (function () {
-  let controller = new ScrollMagic.Controller ();
-  let wipeAnimation = new TimelineMax ()
-    .to ('.moving-words', 1, {y: '-25%'})
-    .to ('.moving-words', 1, {y: '-50%'})
-    .to ('.moving-words', 1, {y: '-75%'});
+$(function () {
+  let controller = new ScrollMagic.Controller();
+  let wipeAnimation = new TimelineMax()
+    .to(".moving-words", 1, { y: "-25%" })
+    .to(".moving-words", 1, { y: "-50%" })
+    .to(".moving-words", 1, { y: "-75%" });
 
-  new ScrollMagic.Scene ({
-    triggerElement: '#moving-texts',
-    triggerHook: 'onLeave',
-    duration: '350%',
+  new ScrollMagic.Scene({
+    triggerElement: "#moving-texts",
+    triggerHook: "onLeave",
+    duration: "350%",
   })
-    .setPin ('#moving-texts')
-    .setTween (wipeAnimation)
-    .addTo (controller);
+    .setPin("#moving-texts")
+    .setTween(wipeAnimation)
+    .addTo(controller);
 
   ////////////////////////////////////////////////
 
-  let tween = TweenMax.from ('.variety-frame', 2, {autoAlpha: 0, scale: 0.1});
-  new ScrollMagic.Scene ({
-    triggerElement: '.trigger-poster',
+  let tween = TweenMax.from(".variety-frame", 2, { autoAlpha: 0, scale: 0.1 });
+  new ScrollMagic.Scene({
+    triggerElement: ".trigger-poster",
     duration: 200,
-    triggerHook: 'onLeave',
+    triggerHook: "onLeave",
   })
-    .setTween (tween)
-    .addTo (controller);
+    .setTween(tween)
+    .addTo(controller);
 
   ////////////////////////////////////////////////
 
   let discover_variety_frame = 1;
   let discover_variety_frame_step = 1;
-  $ ('.pictures-frame').on ('click', function () {
-  // change POSTER
+  $(".pictures-frame").on("click", function () {
+    // change POSTER
     discover_variety_frame++;
-    $ ('.GlobalTour .poster-wrap').addClass ('step-two');
-  // change TEXT
-    $ ('.GlobalTour .all-text-box').addClass ('step-two');
+    $(".GlobalTour .poster-wrap").addClass("step-two");
+    // change TEXT
+    $(".GlobalTour .all-text-box").addClass("step-two");
 
-  // change SLIDE
+    // change SLIDE
     if (
       discover_variety_frame == 3 ||
       discover_variety_frame == 5 ||
       discover_variety_frame == 7
     ) {
-        $ ('.GlobalTour .all-text-box').removeClass ('step-two');
-        $ ('.GlobalTour .poster-wrap').removeClass ('step-two');
+      $(".GlobalTour .all-text-box").removeClass("step-two");
+      $(".GlobalTour .poster-wrap").removeClass("step-two");
 
-        let = icon_vehicle = $ (
-            '.GlobalTour .vehicles-icons .step-' + discover_variety_frame_step
-        )
-        icon_vehicle.removeClass ('step-' + discover_variety_frame_step);
+      let = icon_vehicle = $(
+        ".GlobalTour .vehicles-icons .step-" + discover_variety_frame_step
+      );
+      icon_vehicle.removeClass("step-" + discover_variety_frame_step);
 
-        $ ('.GlobalTour .viriety-posters').removeClass (
-            'frame-' + discover_variety_frame_step
-        );
-        discover_variety_frame_step++;
-        icon_vehicle.addClass ('step-' + discover_variety_frame_step);
+      $(".GlobalTour .viriety-posters").removeClass(
+        "frame-" + discover_variety_frame_step
+      );
+      discover_variety_frame_step++;
+      icon_vehicle.addClass("step-" + discover_variety_frame_step);
 
-        $ ('.GlobalTour .viriety-posters').addClass (
-            'frame-' + discover_variety_frame_step
-        );
+      $(".GlobalTour .viriety-posters").addClass(
+        "frame-" + discover_variety_frame_step
+      );
     }
 
-    console.log (discover_variety_frame);
-    console.log (discover_variety_frame_step);
+    console.log(discover_variety_frame);
+    console.log(discover_variety_frame_step);
   });
 
-  // MANAGERS SECTION 
-    gsap.to(".anime-titles", {
-        x: '',
-        y: '',
-        duration: 1,
-    });
+  // MANAGERS SECTION
+  gsap.to(".anime-titles", {
+    x: "",
+    y: "",
+    duration: 1,
+  });
 
-     ////////////////////////////////////////////////
+  ////////////////////////////////////////////////
 
-    new ScrollMagic.Scene({triggerElement: ".title-managers",offset: 100})
-        .setClassToggle(".title-managers", "go-left") 
-        .addTo(controller);
+  new ScrollMagic.Scene({ triggerElement: ".title-managers", offset: 100 })
+    .setClassToggle(".title-managers", "go-left")
+    .addTo(controller);
 
-    ////////////////////////////////////////////////
+  ////////////////////////////////////////////////
+  // LARGER TITLES
 
-    new ScrollMagic.Scene({
-        triggerElement: "#trigger-no-worries-titles", duration: 2000,offset: "100vh"
-    })
-    .setClassToggle(".title-no-worries", "pinned") 
-    .setPin("#larger-title")
-    .setTween("#larger-title", 0.5, {scale: 4.5, ease: Linear.easeNone}) 
-    .addTo(controller)
+  // new ScrollMagic.Scene({
+  //     triggerElement: "#trigger-no-worries-titles", duration: 2000,offset: 100
+  // })
+  // .setClassToggle(".title-no-worries", "pinned")
+  // .setTween("#larger-title", {scale: 10, ease: Linear.easeNone})
+  // .addTo(controller)
+
+  // COMMING CONTACTS
+
+  // var controllerFuck = new ScrollMagic.Controller({
+  //   globalSceneOptions: {
+  //     triggerHook: 'onLeave',
+  //   }
+  // });
+  // new ScrollMagic.Scene({
+  //     triggerElement: ".no-worries",
+  //   })
+  //   .setPin(".no-worries", {pushFollowers: false}).addIndicators().addTo(controllerFuck);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to("#larger-title", {
+    scrollTrigger: {
+      trigger: "#larger-title",
+      scrub: true,
+      pin: true,
+      start: "center center",
+      end: "+=1000",
+      ease: "power2",
+    },
+    scale: 4.5,
+    opacity: 0,
+    height: 0,
+  });
+
+  gsap.to(".go-connect", {
+    scrollTrigger: {
+      trigger: ".go-connect",
+      scrub: true,
+      pin: true,
+      start: "center center",
+      end: "+=2500",
+      ease: "power2",
+    },
+    opacity:1,
+  });
+
+  gsap.to(".connect-title", {
+    scrollTrigger: {
+      trigger: ".go-connect",
+      scrub: true,
+      // pin: true,
+      start: "center center",
+      end: "+=500",
+      ease: "power2",
+    },
+    y:"-200px",
+  });
+
+  gsap.to(".office-posters-wrap", {
+    scrollTrigger: {
+      trigger: ".go-connect",
+      scrub: true,
+      // pin: true,
+      start: "center center",
+      end: "+=500",
+    },
+    y:"-400px",
+    opacity:1,
+    duration:10,
+    ease: Linear.easeNone
+  });
+
+  gsap.to(".office-2", {
+    scrollTrigger: {
+      trigger: ".office-2",
+      scrub: true,
+      // pin: true,
+      start: "center center",
+      end: "+=500",
+      ease: "power2",
+    },
+    y:"-490px",
+  });
+
+  gsap.to(".office-3", {
+    scrollTrigger: {
+      trigger: ".office-3",
+      scrub: true,
+      // pin: true,
+      start: "center center",
+      end: "+=500",
+      ease: "power2",
+    },
+    y:"-980px",
+  });
+  // gsap.to(".trigger-connect", {
+  //   scrollTrigger: {
+  //     trigger: ".trigger-connect",
+  //     scrub: true,
+  //     // pin: true,
+  //     start: "center center",
+  //     end: "+=1000",
+  //     ease: "power2",
+  //   },
+  //   x: "",
+  //   y: "-500px",
+  //   duration: 20,
+
+  // });
 
 
 
 
   //////////////////////////////////////////////// SLIDER_CAROUSEL
 
-        $(".owl-carousel").owlCarousel();
+  $(".owl-carousel").owlCarousel();
 
- 
+  //////////////////////////////////////////////////// FUNCTION FOR RECIEVE TO ELEMENT SCROLL
 
+  $(window).scroll(function () {
+    let topToElement = $(".no-worries").offset().top,
+      heightElement = $(".no-worries").outerHeight(),
+      windowHeight = $(window).height(),
+      scrollFromTop = $(this).scrollTop();
+    console.log(topToElement, heightElement, windowHeight, scrollFromTop);
+    if (scrollFromTop > topToElement - windowHeight) {
+      // alert('you have scrolled to the h1!');
+    }
+  });
 });
+
+
